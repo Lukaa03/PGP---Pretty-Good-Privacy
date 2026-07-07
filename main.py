088e7ffef754914f66da0185d0245bbcf82363bc
Luka Stojanovic <lukastojanovic401@gmail.com>
2026-07-07 22:47:19 +0200

    zastita privatnog kljuca lozinkom (prsten privatnih kljuceva sa lozinkom)

--- main.py
+++ main.py
@@ -1,51 +1,68 @@
-# # # from crypto.keys import generate_rsa_keypair, public_key_to_pem, pem_to_public_key, calc_key_id
+# # # # # from crypto.keys import generate_rsa_keypair, public_key_to_pem, pem_to_public_key, calc_key_id
+# # # # #
+# # # # # priv = generate_rsa_keypair(2048)
+# # # # # pem = public_key_to_pem(priv.public_key())
+# # # # # print(pem)
+# # # # # pub2 = pem_to_public_key(pem)
+# # # # # print(calc_key_id(pub2))
+# # # #
+# # # # from storage.keyring import PublicKeyRing
+# # # # from crypto.keys import generate_rsa_keypair
+# # # #
+# # # # ring = PublicKeyRing("public_ring.json")
+# # # # priv = generate_rsa_keypair(2048)
+# # # # unos = ring.add_public_key(priv.public_key(), "Luka", "luka@mail.com")
+# # # # print("dodat:",unos["key_id"])
+# # # #
+# # # # ring2 = PublicKeyRing("public_ring.json")
+# # # # print("Posle reload-a ima:", len(ring2.all()), "unosa")
+# # # # print("Nadjen:", ring2.get_by_id(unos["key_id"]) is not None)
+# # #
+# # # from crypto.keys import generate_rsa_keypair, calc_key_id
 # # #
 # # # priv = generate_rsa_keypair(2048)
-# # # pem = public_key_to_pem(priv.public_key())
-# # # print(pem)
-# # # pub2 = pem_to_public_key(pem)
-# # # print(calc_key_id(pub2))
+# # # kid = calc_key_id(priv.public_key())
+# # # print("Key ID:", kid, "| duzina:", len(kid))
+# # # assert len(kid) == 16                      # mora biti 16 hex cifara
+# # #
+# # # priv2 = generate_rsa_keypair(2048)         # drugi kljuc = drugi Key ID
+# # # print("Drugi Key ID:", calc_key_id(priv2.public_key()))
+# # #
+# # # try:                                       # pogresna velicina mora da padne
+# # #     generate_rsa_keypair(512)
+# # #     print("GRESKA: nije bacio izuzetak")
+# # # except ValueError as e:
+# # #     print("Provera velicine radi:", e)
 # #
-# # from storage.keyring import PublicKeyRing
-# # from crypto.keys import generate_rsa_keypair
+# # from crypto.keys import generate_rsa_keypair, calc_key_id, public_key_to_pem, pem_to_public_key
 # #
-# # ring = PublicKeyRing("public_ring.json")
 # # priv = generate_rsa_keypair(2048)
-# # unos = ring.add_public_key(priv.public_key(), "Luka", "luka@mail.com")
-# # print("dodat:",unos["key_id"])
+# # kid = calc_key_id(priv)
+# # print("Key ID:", kid, "| duzina:",len(kid))
 # #
-# # ring2 = PublicKeyRing("public_ring.json")
-# # print("Posle reload-a ima:", len(ring2.all()), "unosa")
-# # print("Nadjen:", ring2.get_by_id(unos["key_id"]) is not None)
+# # pem = public_key_to_pem(priv.publickey())
+# # print(pem[:40])
+# # pub2 = pem_to_public_key(pem)
+# # print("Ispit Key ID posle PEM tuda-nazad:", calc_key_id(pub2)==kid)
+# #
+# # try:
+# #     generate_rsa_keypair(512)
+# # except ValueError as e:
+# #     print("Provara velicine radi:", e)
 #
-# from crypto.keys import generate_rsa_keypair, calc_key_id
+# from storage.keyring import PrivateKeyRing
 #
-# priv = generate_rsa_keypair(2048)
-# kid = calc_key_id(priv.public_key())
-# print("Key ID:", kid, "| duzina:", len(kid))
-# assert len(kid) == 16                      # mora biti 16 hex cifara
+# ring = PrivateKeyRing("private_ring.json")
+# unos = ring.add_new_keypair("Luka","luka@mial.com", 2048, "mojalozinka")
+# print("Dodat privatni kljuc:", unos["key_id"])
 #
-# priv2 = generate_rsa_keypair(2048)         # drugi kljuc = drugi Key ID
-# print("Drugi Key ID:", calc_key_id(priv2.public_key()))
+# print("Sifrovan:", "ENCRYPTED" in unos["enc_private"])
 #
-# try:                                       # pogresna velicina mora da padne
-#     generate_rsa_keypair(512)
-#     print("GRESKA: nije bacio izuzetak")
-# except ValueError as e:
-#     print("Provera velicine radi:", e)
-
-from crypto.keys import generate_rsa_keypair, calc_key_id, public_key_to_pem, pem_to_public_key
-
-priv = generate_rsa_keypair(2048)
-kid = calc_key_id(priv)
-print("Key ID:", kid, "| duzina:",len(kid))
-
-pem = public_key_to_pem(priv.publickey())
-print(pem[:40])
-pub2 = pem_to_public_key(pem)
-print("Ispit Key ID posle PEM tuda-nazad:", calc_key_id(pub2)==kid)
-
-try:
-    generate_rsa_keypair(512)
-except ValueError as e:
-    print("Provara velicine radi:", e)
+# k = ring.load_private_key(unos, "mojalozinka")
+# print("Pristup tacnom lozinkom OK, Key ID:", format(k.n & 0xFFFFFFFFFFFFFFFF, "016x"))
+#
+# try:
+#     ring.load_private_key(unos, "pogrena")
+#     print("Greska: nije odbio pogresnu lozinku")
+# except ValueError:
+#     print("Pogresna lozinka ispravno odbijena")
--- storage/keyring.py
+++ storage/keyring.py
@@ -2,7 +2,10 @@ import os
 import time
 import json
 
-from crypto.keys import calc_key_id, public_key_to_pem, pem_to_public_key
+from Crypto.PublicKey import RSA
+from crypto.keys import calc_key_id, public_key_to_pem, pem_to_public_key, generate_rsa_keypair
+
+# tudji javni kljucevi
 
 class PublicKeyRing:
     def __init__(self, path):
@@ -46,3 +49,55 @@ class PublicKeyRing:
 
     def all(self):
         return self.entries
+
+
+# tvoji parovi kljuceva, tj privatni
+
+class PrivateKeyRing:
+    def __init__(self, path):
+        self.path = path
+        self.entries = []
+        self.load()
+
+    def load(self):
+        if os.path.exists(self.path):
+            with open(self.path, "r", encoding="utf-8") as f:
+                self.entries = json.load(f)
+        else:
+            self.entries = []
+
+    def save(self):
+        with open(self.path, "w", encoding="utf-8") as f:
+            json.dump(self.entries, f, ensure_ascii=False, indent=2)
+
+    def get_by_id(self, key_id):
+        for e in self.entries:
+            if e["key_id"] == key_id:
+                return e
+        return None
+
+    def add_new_keypair(self, name, email, key_size, passphrase):
+        key = generate_rsa_keypair(key_size)
+        enc_private = key.export_key(format='PEM', passphrase=passphrase, pkcs=8)
+        entry = {
+            "timestamp" : time.strftime("%Y-%m-%d %H:%M:%S"),
+            "key_id" : calc_key_id(key),
+            "name": name,
+            "email" : email,
+            "key_size" : key_size,
+            "public_pem" : public_key_to_pem(key.publickey()),
+            "enc_private" : enc_private.decode("ascii")
+        }
+        self.entries.append(entry)
+        self.save()
+        return entry
+
+    def load_private_key(self, entry, passphrase):
+        return RSA.import_key(entry["enc_private"], passphrase=passphrase)
+
+    def delete(self, key_id):
+        self.entries = [e for e in self.entries if e["key_id"]!=key_id]
+        self.save()
+
+    def all(self):
+        return self.enties
